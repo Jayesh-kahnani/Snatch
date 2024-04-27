de3402bcbac1b631433c290ee474b9e77d61c7d1
Jayesh <jayeshkahnani78@gmail.com>
2024-04-27 22:54:14 +0530

    place order

---
+++ src/app/api/place-order/route.ts
@@ -0,0 +1,29 @@
+// src/app/api/place-order/route.ts
+import { NextResponse } from "next/server";
+import prisma from "../../../../lib/prisma";
+
+interface CustomRequest {
+  json: () => any;
+}
+
+export async function POST(request: CustomRequest) {
+  const res = await request.json();
+  const { title, content } = res;
+
+
+  const result = await prisma.post.create({
+    data: {
+      title,
+      content,
+      author: {
+          create: {
+          name: "name will be here",
+            email : "email will be here"
+        },
+        
+      },
+    },
+  });
+
+  return NextResponse.json({ data: result });
+}
--- src/app/place-order/page.tsx
+++ src/app/place-order/page.tsx
@@ -1,3 +1,89 @@
+"use client";
+import { getSession, useSession } from "next-auth/react";
+import { useState } from "react";
+
 export default function Page() {
-  return <p>customers</p>;
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [isSubmitting, setSubmitting] = useState(false);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      // Set the submitting state to true
+      setSubmitting(true);
+
+      const session = await getSession();
+
+      const response = await fetch("/api/place-order", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title, content }),
+      });
+      window.location.reload();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      // Reset the submitting state to false after the fetch completes
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <div className="container mx-auto mt-8">
+      <h1 className="text-3xl font-bold mb-4 text-gray-100 text-center">
+        Place Order
+      </h1>
+      <form
+        onSubmit={handleSubmit}
+        className="max-w-md mx-auto text-gray-600 bg-gray-100 p-8 rounded shadow-md"
+      >
+        <div className="mb-4">
+          <label
+            htmlFor="title"
+            className="block text-gray-600 text-sm font-medium mb-2"
+          >
+            Store from which you wanna order:
+          </label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500 bg-white"
+            required
+          />
+        </div>
+
+        <div className="mb-6">
+          <label
+            htmlFor="content"
+            className="block text-gray-600 text-sm font-medium mb-2"
+          >
+            Describe your order as accurately as possible:
+          </label>
+          <textarea
+            id="content"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            rows={4}
+            className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500 bg-white"
+            required
+          />
+        </div>
+        <div className="flex items-center">
+          <button
+            type="submit"
+            className="bg-blue-500 text-white py-2 px-4 rounded focus:outline-none hover:bg-blue-600"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "Submitting..." : "Submit Order"}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
