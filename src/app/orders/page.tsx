ec719ae8f0f9e3c89877ec2dcfff2e2add1ea121
Jayesh <jayeshkahnani78@gmail.com>
2024-04-27 23:19:43 +0530

    place order

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,3 +1,32 @@
-export default function Page() {
-    return <p>supplier</p>
+import Post from "../ui/order-list"; // Update import path
+import prisma from "../../../lib/prisma";
+
+async function getPosts() {
+  const posts = await prisma.post.findMany({
+    where: { published: true },
+    include: {
+      author: { select: { name: true } },
+    },
+  });
+
+  return posts;
+}
+
+export default async function Page() {
+  const posts = await getPosts();
+
+  return (
+    <>
+      <h1>Your orders</h1>
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          id={post.id}
+          title={post.title}
+          content={post.content}
+          authorName={post.author?.name || "Unknown"}
+        />
+      ))}
+    </>
+  );
 }
--- src/app/user-orders/page.tsx
+++ src/app/user-orders/page.tsx
@@ -1,32 +1,3 @@
-import Post from "../ui/order-list"; // Update import path
-import prisma from "../../../lib/prisma";
-
-async function getPosts() {
-  const posts = await prisma.post.findMany({
-    where: { published: true },
-    include: {
-      author: { select: { name: true } },
-    },
-  });
-
-  return posts;
-}
-
-export default async function Page() {
-  const posts = await getPosts();
-
-  return (
-    <>
-      <h1>Your orders</h1>
-      {posts.map((post) => (
-        <Post
-          key={post.id}
-          id={post.id}
-          title={post.title}
-          content={post.content}
-          authorName={post.author?.name || "Unknown"}
-        />
-      ))}
-    </>
-  );
+export default function Page() {
+  return <div>Your Orders</div>
 }
