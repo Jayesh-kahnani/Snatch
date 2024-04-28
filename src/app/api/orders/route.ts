ad8e6573f625cb53f8d450f7ae77fa3ea90811e3
Jayesh <jayeshkahnani78@gmail.com>
2024-04-28 11:09:45 +0530

    api orders

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -1,11 +1,10 @@
-// src/app/api/order/route.ts
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../../lib/prisma";
-import { getSession } from "next-auth/react"; // Import getSession from next-auth/react
+import { getSession } from "next-auth/react";
 
-export const GET = async (req: NextApiRequest, res: NextApiResponse) =>  {
+export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const session = await getSession({ req }); // Get the session object from the request
+    const session = await getSession({ req });
 
     if (!session) {
       return res.status(401).json({ error: "Unauthorized" });
@@ -15,7 +14,7 @@ export const GET = async (req: NextApiRequest, res: NextApiResponse) =>  {
       return res.status(405).json({ error: "Method Not Allowed" });
     }
 
-    const userId = session.user?.email; // Get the user's email from the session
+    const userId = session.user?.email;
 
     if (!userId) {
       return res.status(400).json({ error: "User ID not found in session" });
@@ -23,8 +22,8 @@ export const GET = async (req: NextApiRequest, res: NextApiResponse) =>  {
 
     const orders = await prisma.post.findMany({
       where: {
-        author: { email: userId }, // Filter orders by user's email
-        published: true, // Filter based on your criteria
+        author: { email: userId },
+        published: true,
       },
       include: {
         author: { select: { name: true } },
@@ -36,4 +35,4 @@ export const GET = async (req: NextApiRequest, res: NextApiResponse) =>  {
     console.error("Error fetching orders:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
-}
+};
